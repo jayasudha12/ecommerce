4463956b4032578e45a01f0c19958f48dc872079
JAYASUDHA T <jayasudha.t2022cse@sece.ac.in>
2024-08-08 10:46:43 +0530

    basic setup

--- routes/Cartrouter.js
+++ routes/Cartrouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const cartcontroller = require('../controller/CartController');
+const auth = require('../middleware/auth');
 
-router.post('/addtocart',cartcontroller.addtocart);
-router.get('/getitem',cartcontroller.getitem);
-router.delete('/deletecartproduct/:id',cartcontroller.deleteproduct);
+router.post('/addtocart',auth,cartcontroller.addtocart);
+router.get('/getitem',auth,cartcontroller.getitem);
+router.delete('/deletecartproduct/:id',auth,cartcontroller.deleteproduct);
 
 module.exports = router;
--- routes/Productrouter.js
+++ routes/Productrouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const productcontroller = require('../controller/ProductController')
+const auth = require("../middleware/auth")
 
 router.get('/products',productcontroller.getallproducts);
-router.post('/addproduct',productcontroller.addproduct);
-router.patch('/editproduct/:id',productcontroller.editproduct);
-router.delete('/deleteproduct/:id',productcontroller.deleteproduct);
+router.post('/addproduct',auth,productcontroller.addproduct);
+router.patch('/editproduct/:id',auth,productcontroller.editproduct);
+router.delete('/deleteproduct/:id',auth,productcontroller.deleteproduct);
 
 module.exports = router;
